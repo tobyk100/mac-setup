840847e7f1144bbeec39bd61b25dc7f2f067ef4f
Toby <tobyk100@gmail.com>
2014-10-06 17:33:51 -0700

    Added feedback.

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if hash brew 2>/dev/null; then
+  echo "Brew installed, moving on."
+else
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+git config --global user.name "Toby"
+git config --global user.email tobyk100@gmail.com
+
+echo "git name:" `git config --get user.name`
+echo "git email:" `git config --get user.email`
